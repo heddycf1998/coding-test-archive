8ccd4a92820626d4ff96d6508c60b9150785a906
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-02-17 06:31:29 +0700

    11 done and environment

---
+++ codewars/javascript/11_opposite.js
@@ -0,0 +1,36 @@
+// Very simple, given a number, find its opposite (additive inverse).
+// Examples:
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+  return Number(number) * (-1) || 0
+}
+
+//Sample Test :
+import { assert } from 'chai';
+
+describe('Fixed tests', () => {
+  it('Is it a function?', () => {
+    assert.strictEqual(typeof opposite, 'function', 'opposite should be a function');
+  });
+  it('should return -1', () => {
+    assert.strictEqual(opposite(1), -1);
+  });
+  it('should return 0', () => {
+    assert.strictEqual(opposite(0), 0);
+  });
+  it('should return -4.25', () => {
+    assert.strictEqual(opposite(4.25), -4.25);
+  });
+  it('should return -3.3333333', () => {
+    assert.strictEqual(opposite(3.3333333), -3.3333333);
+  });
+  it('should return 12525220.3325', () => {
+    assert.strictEqual(opposite(-12525220.3325), 12525220.3325);
+  });
+  it('should return 5', () => {
+    assert.strictEqual(opposite(-5), 5);
+  });
+});
