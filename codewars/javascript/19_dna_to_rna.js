b07e3cab3bbf197abb9a2ecf5cd16db2153aa340
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-02-21 05:40:03 +0700

    19 done

---
+++ codewars/javascript/19_dna_to_rna.js
@@ -0,0 +1,30 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+// Create a function which translates a given DNA string into RNA.
+// For example:
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+  let dnaSplit = dna.split('')
+  for (let i = 0; i < dnaSplit.length; i++) {
+    if (dnaSplit[i] === 'T') {
+      dnaSplit[i] = 'U'
+    }
+  }
+  return dnaSplit.join('')
+}
+
+
+
+
+// Sample Test :
+import { assert } from "chai";
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(DNAtoRNA("TTTT"), "UUUU")
+    assert.strictEqual(DNAtoRNA("GCAT"), "GCAU")
+    assert.strictEqual(DNAtoRNA("GACCGCCGCC"), "GACCGCCGCC")
+    });
+  })
