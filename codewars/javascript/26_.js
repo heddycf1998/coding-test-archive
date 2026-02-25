7314d2e00ec1468f128abdb0006e43c26ca1a9d5
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-02-25 22:45:58 +0700

    26 ngantuk

---
+++ codewars/javascript/26_.js
@@ -0,0 +1,9 @@
+function validatePIN (pin) {
+   let panjangPin = pin.toString().length
+   let convertNumber = Number(pin)
+   console.log(parseInt(pin))
+
+}
+
+// validatePIN(12345)
+validatePIN('0000')
