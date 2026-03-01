7a1847fc68e351b6d784603b7e09b5ccad4c5289
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-03-02 05:37:19 +0700

    34 done

---
+++ codewars/javascript/34_function_1.js
@@ -0,0 +1,15 @@
+// Write a function "greet" that returns "hello world!"
+function greet() {
+  return "hello world!"
+}
+
+import { assert } from "chai";
+
+describe("Testing function", function() {
+  it("Is it a function?", function() {
+    assert.strictEqual(typeof greet, "function", "greet should be a function");
+  });
+  it("Correct return-value?", function() {
+    assert.strictEqual(greet(), "hello world!");
+  });
+});
