03c4cf8e4daf57993687a082f3126701e702d450
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-03-13 23:19:03 +0700

    48 still progress

---
+++ codewars/javascript/48_.js
@@ -0,0 +1,26 @@
+function duplicateCount(text){
+    if (text === "") {
+        console.log(0)
+    }
+
+    const alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+    for (let i = 0; i < text.length; i++) {
+        
+    }
+    let count = 0
+    for (let j = 0; j < alphabet.length; j++) {
+        for (let i = 0; i < text.length; i++) {
+            if (text[i].includes(alphabet[j])) {
+                console.log(count++)
+            }
+        }
+    }
+}
+
+
+duplicateCount("")
+duplicateCount("abcde")
+// duplicateCount("aabbcde")
+// duplicateCount("aabBcde")
+// duplicateCount("Indivisibility")
+// duplicateCount("Indivisibilities") 
