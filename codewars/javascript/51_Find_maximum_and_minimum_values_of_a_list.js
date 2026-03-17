33564deaa8cd4931f348906342faa62041dada42
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-03-18 05:22:02 +0700

    51 done

---
+++ codewars/javascript/51_Find_maximum_and_minimum_values_of_a_list.js
@@ -0,0 +1,47 @@
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively. Each function returns one number.
+
+// Examples (Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+
+var min = function(list){
+    for (let i = 0; i < list.length; i++) {
+      for (let j = i + 1; j < list.length; j++) {
+        if (list[i] > list[j]) {
+          let temp = list[i]
+          list[i] =  list[j]
+          list[j] = temp
+        }
+      }
+    }
+    return list[0];
+}
+
+var max = function(list){
+    for (let i = 0; i < list.length; i++) {
+      for (let j = i + 1; j < list.length; j++) {
+        if (list[i] < list[j]) {
+          let temp = list[i]
+          list[i] =  list[j]
+          list[j] = temp
+        }
+      }
+    }
+    return list[0];
+}
+
+
+
+// Sample Test :
+import { assert } from "chai";
+
+describe("Test", function(){
+  it("Examples", function(){
+    assert.strictEqual(min([-52, 56, 30, 29, -54, 0, -110]), -110);
+    assert.strictEqual(min([42, 54, 65, 87, 0]), 0);
+    assert.strictEqual(max([4,6,2,1,9,63,-134,566]), 566);
+    assert.strictEqual(max([5]), 5);
+  });
+});
