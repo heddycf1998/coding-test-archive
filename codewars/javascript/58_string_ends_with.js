8a28cffb7672daadbfd88f8ecf1fc9aed181a64f
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-03-28 21:06:10 +0700

    58 done

---
+++ codewars/javascript/58_string_ends_with.js
@@ -0,0 +1,27 @@
+function solution(str, ending){
+  if (str.endsWith(ending)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+
+// Sample Test :
+import { assert } from "chai";
+
+describe("solution", function() {
+  it("Sample Tests", function() {
+    tester('abcde', 'cde', true);
+    tester('abcde', 'abc', false);
+    tester('empty ending', '', true);
+    tester('', 'empty string', false);
+    tester('', '', true);
+  });
+
+  function tester(str, ending, expected) {
+    const err_msg = `Failed for inputs:\n- str: "${str}"\n- ending: "${ending}"\n\n`;
+    assert.strictEqual(solution(str, ending), expected, err_msg);
+  };
+});
