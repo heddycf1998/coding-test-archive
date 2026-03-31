634b3047a5a5171e749f0d9a6e6bcb526089459c
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-03-31 21:41:02 +0700

    60 done still progress

---
+++ codewars/javascript/60_count_the_divisors_of_a_number.js
@@ -0,0 +1,38 @@
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000, but fixed tests go higher.
+
+// Examples (input --> output)
+// 4 --> 3 // we have 3 divisors - 1, 2 and 4
+// 5 --> 2 // we have 2 divisors - 1 and 5
+// 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+// 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+// Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+
+function getDivisorsCnt(n){
+    let temp = 0
+    for (let i = 1; i <= n; i ++) {
+      if (n === 0) {
+        return temp
+      }
+        if (n % i === 0) {
+          temp = temp + 1
+        }
+    }
+  return temp
+}
+
+
+// Sample Test :
+import { assert } from "chai";
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(getDivisorsCnt(1),  1);
+    assert.strictEqual(getDivisorsCnt(10), 4);
+    assert.strictEqual(getDivisorsCnt(11), 2);
+    assert.strictEqual(getDivisorsCnt(54), 8);
+    assert.strictEqual(getDivisorsCnt(100), 8);
+  });
+});
