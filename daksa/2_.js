3489c53ed306673b87216c2b4bffca17bc3c8d75
Heddy Cahya Firdaus <heddycf1998@gmail.com>
2026-04-03 20:42:57 +0700

    Daksa 2 ingatan memory otaku

---
+++ daksa/2_.js
@@ -0,0 +1,72 @@
+function createAccount() {
+    let nomorAccount
+    let nama
+    let nik
+    let statusActive
+
+    // setter & getter
+
+
+}
+
+// function enum statusActive() {
+//     INACTIVE, ACTIVE
+// }
+
+// function account() {
+//     let nomorAccount
+//     let nama
+//     let nik
+//     let active
+
+//     // setter & getter
+// }
+
+// 1. saat user memasukkan nik maka yang keluar malahan nik itu sendiri seharusnya tidak
+// 2. pada saat kirim nama malah yang keluar nik seharusnya tidak keluar nama
+// 3. user menuliskan nik sebanyak 16 karakter tapi gak bisa buat akun, 16 karakter tersebut adalah emang harus 16 karakter
+// 4. banyaknya list akun tapi malah force close atau hang, seharusnya bisa tahu berapa banyak list akun
+
+// function throwApaGitu() {
+//     if (createAccount === null || account.isempty()) {
+//         throw exception ("Akun tidak boleh kosong")
+//     }
+
+//     if (createAccount.name === null) {
+//         throw exception ("Nama akun tidak ada")
+//     }
+
+//     if (createAccount.nik === null || size.createAccount.nik === 16) {
+//         throw exception ("Nomor NIK tidak ada")
+//     }
+
+//     let account = createAccount()
+//     account.createNomorAccount(getNomorAccount)
+//     account.createNama(getNama)
+//     account.createNik(getNIK)
+//     account.createStatusActice(getStatusActive)
+
+
+// }
+
+// function banyaknyaAkun(list<Account> account) {
+//     let sizeaccount = size.account
+//     let valid = true
+//     let counter = 0
+//     while () {
+//         counter = counter + 1
+//         valid
+//     }
+
+//     if (ada akunnya maka valid) {
+//         return valid
+//     }
+
+//     if (ada nik maka valid) {
+//         return valid
+//     }
+
+//     if (ada namanya maka valid) {
+//         return valid
+//     }
+// }
